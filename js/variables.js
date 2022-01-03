5061cfc1171ee012b691836cb9a4072d2d8951ad
Riza P Nansuri <riza.nansuri@dana.id>
2022-01-03 11:02:10 +0700

    add some changes

--- js/variables.js
+++ js/variables.js
@@ -3,7 +3,7 @@ const configData = {
     env: 'https://nansuri.tunnelto.dev/',
     loginApi: 'apis/user/login',
     getDataApi: 'apis/invitation/attendee/query?invitationid=',
-    invitationId: 'GREATESTGIFT01',
+    invitationId: localStorage.getItem('invitation_id') ,
     customHeaders: {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer ' + localStorage.getItem('token') 
